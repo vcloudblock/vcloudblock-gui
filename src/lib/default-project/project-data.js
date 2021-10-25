13a89b8a16f7c30a612e390ca8b73d68476681f7
ArthurZheng <869705086@qq.com>
2021-10-25 16:42:35 +0800

    Corrected the coordinates of the center of rotation of the demon bird

--- src/lib/default-project/project-data.js
+++ src/lib/default-project/project-data.js
@@ -74,8 +74,8 @@ const projectData = translateFunction => {
                         bitmapResolution: 1,
                         md5ext: '5de48e72637db4d7fc7f6bb526354114.svg',
                         dataFormat: 'svg',
-                        rotationCenterX: 48,
-                        rotationCenterY: 50
+                        rotationCenterX: 93,
+                        rotationCenterY: 80
                     },
                     {
                         assetId: '48bbc82a10af3d0e0b7d5c677097df0e',
@@ -83,8 +83,8 @@ const projectData = translateFunction => {
                         bitmapResolution: 1,
                         md5ext: '48bbc82a10af3d0e0b7d5c677097df0e.svg',
                         dataFormat: 'svg',
-                        rotationCenterX: 46,
-                        rotationCenterY: 53
+                        rotationCenterX: 93,
+                        rotationCenterY: 80
                     }
                 ],
                 sounds: [
